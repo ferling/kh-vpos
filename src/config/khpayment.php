97e18549d4acf64c3751470651d10281d34426d8
Zoltan Tolnai <tolnaiz@gmail.com>
2022-07-29 15:14:20 +0200

    Update khpayment.php

--- src/config/khpayment.php
+++ src/config/khpayment.php
@@ -1,9 +1,9 @@
 <?php 
 
 return [
-	'mid' => ENV('KH_MERCHANT_ID', 0),
-	'currency' => ENV('KH_CURRENCY','HUF'),
-	'private_key' => env('KH_PRIVATE_KEY','path/to/privatekey.pem')
+	'mid' => env('KH_MERCHANT_ID', 0),
+	'currency' => env('KH_CURRENCY','HUF'),
+	'private_key' => env('KH_PRIVATE_KEY','path/to/privatekey.pem'),
 	'live' => env('KH_LIVE',false),
 	'check_signature' => env('KH_CHECK_SIGNATURE',false),
 	'lang' => env('KH_LANG','HU')
